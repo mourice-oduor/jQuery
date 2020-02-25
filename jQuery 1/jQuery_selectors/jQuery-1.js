d6e81fc2d8fc93ef582f51613b401bc44cc6fa24
mourice-oduor <otienomaurice364@gmail.com>
2020-02-24 21:24:17 -0500

    jQuery selectors

---
+++ jQuery 1/jQuery_selectors/jQuery-1.js
@@ -0,0 +1,11 @@
+//selecting all the divs and give them a purple color
+$('div').css('background', 'purple')
+
+//selecting the divs with class'highlight' and make them 200px wide
+$('.highlight').css('width', '200px')
+
+//select the div with id 'third' and give it an orange border
+$('#third').css('border', '4px solid orange')
+
+//select the first div only and change it's color to pink
+$('div:first-of-type').css('color','pink')
