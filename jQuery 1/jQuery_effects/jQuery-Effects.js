172e6e7b04b010766c04d0ffa60b09efff9af493
mourice-oduor <otienomaurice364@gmail.com>
2020-02-24 21:25:06 -0500

    jQuery effects

---
+++ jQuery 1/jQuery_effects/jQuery-Effects.js
@@ -0,0 +1,10 @@
+$("button").on("click",function(){
+    $('div').fadeOut(1000, function(){
+        console.log("Fade Completed")
+    })
+})
+
+//fadein fadeToggle
+//slideup 
+//slidedown
+//slideToggle
